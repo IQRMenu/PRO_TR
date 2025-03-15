054675e14155015df1eeaf7a5646a8a8b9b87a4d
IQRMenu <interactivemenuqr@gmail.com>
2025-03-15 20:07:43 -0300

    ProTR

---
+++ js/globalData.js
@@ -0,0 +1,21 @@
+export let globalData = {
+  cafeName: 'MenuProTurkey',
+  sheetId: '1rquPs2dI-Z_TGAeNCnWSyqHlqEswyywWZaA7RCsFUoo',
+  mainLang: 'tr',
+  currencySymbol: '₺',
+  googleLink: 'https://maps.app.goo.gl/JN7ToTsjgCQW9FtY8',
+  version: 'pro',//pro o basik
+
+  fotmAction: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSePk5-qlP6l6Ued7pi8q5A3RfSUIcY4vqUwjYi1TlIDEM-BqA/formResponse",
+  inputNames: {
+    inputOrderId: "entry.1753983149",
+    inputLangOrderTable: "entry.868338239",
+    inputTableNumberOrderTable: "entry.814736468",
+    inputVisitorTypeOrderTable: "entry.1357817951",
+    inputDishesOrderTable: "entry.993484371",
+    inputTotolCostOrderTable: "entry.728135158",
+    inputType: "entry.968086505",
+    
+  }
+
+}
